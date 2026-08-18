f22ddcd84a717310f3193db2ea2ae3a23d91cc10
TrongPhuc1609 <ngotrongphuc2016.tb@gmail.com>
2026-08-18 14:43:44 +0700

    chore: expose vision adapters

---
+++ src/vision/adapters/__init__.py
@@ -0,0 +1,6 @@
+from .base import VisionAdapter, VisionInput
+from .detection import DetectionAdapter
+from .classification import ClassificationAdapter
+from .segmentation import SegmentationAdapter
+from .anomaly import AnomalyDetectionAdapter
+from .factory import create_adapter
